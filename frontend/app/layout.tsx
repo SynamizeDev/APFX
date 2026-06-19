682a01d53c048a56414cad0b50afc69fb047e3c9
JatinPrajapat <jatinprajapat028@gmail.com>
2026-06-19 16:02:27 +0530

    Fix: favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/apfx-icon.ico', sizes: 'any' },
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/assets/apfx-icon.png', type: 'image/png', sizes: '32x32' },
     ],
     apple: [
