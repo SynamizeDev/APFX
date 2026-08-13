c3d8cb1f41feeb119a73a88cde23226720f2ef1f
Jay Prajapat <jayprajapat014@gmail.com>
2026-08-13 16:52:07 +0530

    FAVICON

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -66,6 +66,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
       { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
       { url: "/android-chrome-512x512.png", type: "image/png", sizes: "512x512" }
